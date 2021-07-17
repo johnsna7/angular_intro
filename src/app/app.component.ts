c61cd7819e8ebe0052e13a9a6d161988c833582c
Nathan Johnson <johnsna7@oregonstate.edu>
2021-07-17 16:20:14 -0500

    can add items to list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	  selector: 'app-root',
+	    templateUrl: './app.component.html',
+	      styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
@@ -12,16 +12,23 @@ export class AppComponent {
 	filter: 'all' | 'active' | 'done' = 'all';
 
 	allItems = [
-		{ description: 'eat', done: true },
-		{ description: 'sleep', done: false},
-		{ description: 'play', done: false},
-		{ description: 'laugh', done: false},
+	        { description: 'eat', done: true },
+		{ description: 'sleep', done: false },
+		{ description: 'play', done: false },
+		{ description: 'laugh', done: false },
 	];
 
 	get items() {
 		if (this.filter === 'all') {
-			return this.allItems;
+	        	return this.allItems;
 		}
-		return this. allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+		return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+	}
+
+	addItem(description) {
+		this.allItems.unshift({
+			description,
+		        done: false
+		});
 	}
 }
